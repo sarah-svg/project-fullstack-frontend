552adeb932fb5f705456ba2f92dc5b65680b75aa
sarah-svg <kjskillen@gmail.com>
2021-10-12 20:26:18 -0700

    front end int commit

---
+++ src/components/app/Crud.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Crud(props) {
+  return (
+    <div>
+            
+    </div>
+  );
+}
+
+Crud.propTypes = {
+
+};
+
+export default Crud;
+
---
+++ src/components/app/Result.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+// eslint-disable-next-line no-unused-vars
+import PropTypes from 'prop-types';
+
+function Result(props) {
+  return (
+    <div>
+            
+    </div>
+  );
+}
+
+Result.propTypes = {
+
+};
+
+export default Result;
+
