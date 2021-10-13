c6e4c0e2f01deca1205f4520f0a3c7359616c7e9
sarah-svg <kjskillen@gmail.com>
2021-10-12 20:57:14 -0700

    crud file and result file wrote

--- src/components/app/Map.jsx
+++ src/components/app/Map.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 
 export default function Map() {
+  state = {
+    image: '',
+    location: '',
+    method: '',
+    response: {},
+    id: ''
+  };
+
+
   return (
     <div>
             
--- src/components/app/Result.jsx
+++ src/components/app/Result.jsx
@@ -2,17 +2,22 @@
 import React from 'react';
 // eslint-disable-next-line no-unused-vars
 import PropTypes from 'prop-types';
+import ReactJson from 'react-json-view';
 
-function Result(props) {
+function Result({ response }) {
   return (
     <div>
-            
+      <ReactJson src={response} />     
     </div>
   );
 }
 
-Result.propTypes = {
 
+Result.propTypes = {
+  response: PropTypes.oneOfType([
+    PropTypes.arrayOf,
+    PropTypes.object
+  ])
 };
 
 export default Result;
