f2baf45c21095b69d6b8635128a8426e634ce86c
sarah-svg <kjskillen@gmail.com>
2021-10-14 15:19:24 -0700

    testing all my presentational components

--- src/components/app/Crud.test.jsx
+++ src/components/app/Crud.test.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
-import App from './App';
+
 import { MemoryRouter } from 'react-router';
+import Crud from './Crud';
 
 describe('App component', () => {
   afterEach(() => cleanup());
   it('renders App', () => {
-    const { asFragment } = render(<MemoryRouter> <App /></MemoryRouter>);
+    const { asFragment } = render(<MemoryRouter> <Crud/></MemoryRouter>);
     expect(asFragment()).toMatchSnapshot();
   });
 });
